6169c4526faa4ffbad18b2296c48080e2ca22154
William <nearata@protonmail.com>
2024-03-28 21:29:10 +0100

    unlimited

--- src/main/java/com/github/neapovil/dynamicslots/DynamicSlots.java
+++ src/main/java/com/github/neapovil/dynamicslots/DynamicSlots.java
@@ -1,8 +1,11 @@
 package com.github.neapovil.dynamicslots;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class DynamicSlots extends JavaPlugin
+public final class DynamicSlots extends JavaPlugin implements Listener
 {
     private static DynamicSlots instance;
 
@@ -10,10 +13,23 @@ public final class DynamicSlots extends JavaPlugin
     public void onEnable()
     {
         instance = this;
+
+        this.getServer().setMaxPlayers(0);
+
+        this.getServer().getPluginManager().registerEvents(this, this);
     }
 
     public static DynamicSlots instance()
     {
         return instance;
+    }
+
+    @EventHandler
+    private void onPlayerLogin(PlayerLoginEvent event)
+    {
+        if (event.getResult().equals(PlayerLoginEvent.Result.KICK_FULL))
+        {
+            event.setResult(PlayerLoginEvent.Result.ALLOWED);
+        }
     }
 }
